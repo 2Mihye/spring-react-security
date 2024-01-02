e07923ee1c908514e60960170d8ab71855066c7b
Kaye <139112573+2Mihye@users.noreply.github.com>
2024-01-02 11:50:08 +0900

    Java & React Connect

---
+++ client/src/App.js
@@ -0,0 +1,107 @@
+import "./App.css";
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+
+function App() {
+  const [data, setData] = useState([]);
+  const [newMember, setNewMember] = useState({ member_name: "", email: "" });
+
+  // fetchData라는 이름을 정의하여 try-catch 문을 사용해 비동기작업 중 발생하는 에러를 잡아내고 콘솔에 메세지를 출력하는 것
+  // 간접적으로 호출
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const res = await axios.get("http://localhost:8080/api/member", {
+          withCredentials: true,
+        });
+        setData(res.data);
+      } catch (error) {
+        console.log(error);
+      }
+    };
+    fetchData();
+  }, []);
+
+  // 데이터 작성한 내용으로 변경하는 함수
+  // e : even의 약자
+  const handleInputChange = (e) => {
+    const { name, value } = e.target;
+    setNewMember((prevMember) => ({ ...prevMember, [name]: value })); // prevMember을 복제해서 name을 내가 쓴 값으로 설정하는 것
+  };
+
+  // 데이터 보내줄 버튼함수 post 추가
+  const handleAddMember = async () => {
+    try {
+      const response = await axios.post(
+        "http://localhost:8080/api/member",
+        newMember,
+        {
+          withCredentials: true,
+        }
+      );
+      // 변경된 데이터 값 저장
+      setData((prevMember) => [...prevMember, response.data]);
+
+      // 데이터 저장된 후 빈 값으로 초기화 하기 원한다면 초기화도 진행
+      setNewMember({ member_name: "", email: "" });
+    } catch (error) {
+      console.error("데이터가 부적합합니다.", error);
+    }
+  };
+
+  /*
+  직접적으로 한 번에 axios를 호출하는지 차이가 있을 뿐 위 코드와 아래 코드는 같은 것임.
+
+
+
+  useEffect 안에서 직접 비동기 작업을 수행하고 간단하게 catch문을 사용하여 에러를 처리하고 콘솔에 에러 메세지를 출력
+
+  useEffect(() => {
+    axios
+      // API 주소값을 가져옴.
+      .get("http://localhost:8080/api/expression", { withCredentials: true })
+      // response와 res는 같은 의미이며, 안에 변수값은 정해진 변수값은 없지만 되도록이면 res response를 사용하면 더 좋음 !
+      .then((res) => {
+        setData(res.data);
+      })
+      .catch((error) => {
+        console.log("데이터없음", error);
+      });
+    // 처음에만 가져올 것이기 때문에 [] 공백을 넣어줌.
+  }, []);
+  */
+  return (
+    <div>
+      <h1>API 호출 확인</h1>
+      <ul>
+        {data.map((member) => (
+          <li key={member.id}>
+            {member.membername} = {member.email}
+          </li>
+        ))}
+      </ul>
+      <h2>새로운 회원 저장</h2>
+      <div>
+        <label>회원 이름 : </label>
+        <input
+          type="text"
+          name="member_name"
+          value={newMember.member_name}
+          onChange={handleInputChange}
+        />
+      </div>
+      <div>
+        <label>회원 이메일 : </label>
+        <input
+          type="text"
+          name="email"
+          value={newMember.email}
+          onChange={handleInputChange}
+        />
+      </div>
+      <button onClick={handleAddMember}>회원 저장</button>
+    </div>
+  );
+}
+
+export default App;
---
+++ springChap1/src/main/java/com/kh/springChap1/controller/MemberController.java
@@ -0,0 +1,33 @@
+package com.kh.springChap1.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import com.kh.springChap1.model.Member;
+import com.kh.springChap1.repository.MemberRepository;
+
+@RestController
+@RequestMapping("/api/member")
+@CrossOrigin(origins = "http://localhost:3000", allowCredentials = "true")
+public class MemberController {
+	@Autowired
+	private MemberRepository memberRepository;
+	
+	@GetMapping // ("/api/member") GetMapping 뒤에 아무것도 없으면 위에서 작성해준 ("/api/member")로 자동으로 지정\
+	public List<Member> getAllUsers(){
+		return memberRepository.findAll();
+	}
+	
+	/*
+	 @PostMapping : 클라이언트(사용자)가 html에 작성한 정보를 DB에 저장할 수 있도록 도와주는 어노테이션
+	 ResponseEntity : 응답을 나타내는 클래스로 우리가 자주봤던 404, 400, 500(에러) 이외에 200이라는 응답이 있는데, 이는 성공적으로 데이터를 전송했다는 의미.
+	 				  ResponseEntity.ok 라는 것은 데이터를 잘 전송했다는 의미이므로 ok가 200이라는 내용을 담고있음.
+	 */
+	@PostMapping
+	public ResponseEntity<Member> createMember (@RequestBody Member newMember) {
+		Member createdMember = memberRepository.save(newMember);
+		return ResponseEntity.ok(createdMember);
+	}
+}
