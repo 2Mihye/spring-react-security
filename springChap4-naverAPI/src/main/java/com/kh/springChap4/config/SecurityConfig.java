373f9ae163baedcdb028a710512a63476e764d5e
Kaye <139112573+2Mihye@users.noreply.github.com>
2024-01-04 17:30:08 +0900

    Naver & Google Login

--- springChap4-naverAPI/src/main/java/com/kh/springChap4/config/SecurityConfig.java
+++ springChap4-naverAPI/src/main/java/com/kh/springChap4/config/SecurityConfig.java
@@ -32,25 +32,12 @@ public class SecurityConfig {
 	}
 	
 	// 추후 네이버 등록한 정보(naverClientRegistration()) 저장하기 위한 공간(InMemoryClientRegistrationRepository) 생성
-	@Bean
-	public ClientRegistrationRepository clientRegistrationRepository() {
-		return new InMemoryClientRegistrationRepository(naverClientRegistration());
-	}
-	
-	// 네이버 클라이언트의 등록 정보를 생성하는 메서드로 클라이언트 아이디와 시크릿, 인증 후 리다이렉트 URI 설정
-	public ClientRegistration naverClientRegistration() {
-		return ClientRegistration.withRegistrationId("naver")
-				.clientId("tmfesNnXtMZzYFKN7Xuu") // .clientId("https://developers.naver.com/apps/#/myapps/ 안에 적혀있는 Client ID 가져오기")
-				.clientSecret("eEfdEaY3rJ") // .clientId("https://developers.naver.com/apps/#/myapps/ 안에 적혀있는 Client Secret 가져오기")
-				.redirectUri("http://localhost:8080/login/oauth2/code/naver") // 네이버에서도 인증 후 OAuth2 엔드포인트설정
-				.clientName("Naver")
-				.authorizationUri("https://nid.naver.com/oauth2.0/authorize")
-				.tokenUri("https://nid.naver.com/oauth2.0/token")
-				.userInfoUri("https://openapi.naver.com/v1/nid/me")
-				.userNameAttributeName("response")
-				.authorizationGrantType(AuthorizationGrantType.AUTHORIZATION_CODE)
-				.build();
-	}
+    @Bean
+    public ClientRegistrationRepository clientRegistrationRepository() {
+        return new InMemoryClientRegistrationRepository(
+                naverClientRegistration(),
+                googleClientRegistration());
+    }
 	
 	// 인증 통합 관리하는 매니저
 	@Bean
@@ -68,6 +55,41 @@ public class SecurityConfig {
 		authorizedClientManager.setAuthorizedClientProvider(authorizedClientProvider);
 		
 		return authorizedClientManager;
+	}
+	
+	
+	// 네이버 클라이언트의 등록 정보를 생성하는 메서드로 클라이언트 아이디와 시크릿, 인증 후 리다이렉트 URI 설정
+	public ClientRegistration naverClientRegistration() {
+		return ClientRegistration.withRegistrationId("naver")
+				.clientId("tmfesNnXtMZzYFKN7Xuu") // .clientId("https://developers.naver.com/apps/#/myapps/ 안에 적혀있는 Client ID 가져오기")
+				.clientSecret("eEfdEaY3rJ") // .clientId("https://developers.naver.com/apps/#/myapps/ 안에 적혀있는 Client Secret 가져오기")
+				.redirectUri("http://localhost:8080/login/oauth2/code/naver") // 네이버에서도 인증 후 OAuth2 엔드포인트설정
+				.clientName("Naver")
+				.authorizationUri("https://nid.naver.com/oauth2.0/authorize")
+				.tokenUri("https://nid.naver.com/oauth2.0/token")
+				.userInfoUri("https://openapi.naver.com/v1/nid/me")
+				.userNameAttributeName("response")
+				.authorizationGrantType(AuthorizationGrantType.AUTHORIZATION_CODE)
+				.build();
+	}
+	
+
+	
+	// 구글 클라이언트의 등록 정보를 생성하는 메서드로 클라이언트 아이디와 시크릿, 인증 후 리다이렉트 URI 설정
+	public ClientRegistration googleClientRegistration() {
+		return ClientRegistration.withRegistrationId("google")
+				.clientId("664155680404-ohm9nk0kococihre5o6gi1vv428r9fde.apps.googleusercontent.com")
+				.clientSecret("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")
+				.redirectUri("http://localhost:8080/login/oauth2/code/google") // 네이버에서도 인증 후 OAuth2 엔드포인트설정
+				.clientName("Google")
+				.authorizationUri("https://accounts.google.com/o/oauth2/auth")
+				.tokenUri("https://www.googleapis.com/oauth2/v4/token")
+				.userInfoUri("https://www.googleapis.com.oauth2/v3/userinfo")
+				.userNameAttributeName("sub")
+				.authorizationGrantType(AuthorizationGrantType.AUTHORIZATION_CODE)
+				.scope("openid", "profile", "email")
+				.jwkSetUri("https://www.googleapis.com/oauth2/v3/certs")
+				.build();
 	}
 	
 }
