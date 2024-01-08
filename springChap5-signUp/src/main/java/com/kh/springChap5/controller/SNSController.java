2ac7fde16774821a2e70543ce0ad8d89f6e6bc18
Kaye <139112573+2Mihye@users.noreply.github.com>
2024-01-08 17:21:16 +0900

    SNS Login

--- springChap5-signUp/src/main/java/com/kh/springChap5/controller/SNSController.java
+++ springChap5-signUp/src/main/java/com/kh/springChap5/controller/SNSController.java
@@ -20,8 +20,17 @@ public class SNSController {
 	@GetMapping("/login/oauth2/code/naver")
 	public String naverLoginCallback(@AuthenticationPrincipal OAuth2User oauth2User, Model model) {
 		Object response = oauth2User.getAttribute("response");
-		// instanceof : 객체가 인스턴스인지 아닌지 판단해줌.
+		/*
+		 instanceof : 객체가 인스턴스인지 아닌지 판단해줌.
+		 oauth2 인증에서 사용자 정보가 KEY value로 해서 Map과 유사한 구조로 캡슐화(보호)가 된다.
+		 response로 값을 가져오는데 Map에 인스턴스인지를 확인해보는 것
+		 만약 response가 Map의 구조로 되어있다면 email이나 name의 정보를 추출하여 원하는 정보를 가져오겠다는 의미
+		*/
 		if(response instanceof Map) {
+			Map<String, Object> responseMap = (Map<String, Object>) response;
+			String email = (String)responseMap.get("email");
+			String name = (String)responseMap.get("name");
+			
 			Member naverMember = new Member();
 			naverMember.setEmail(email);
 			naverMember.setFullName(name);
