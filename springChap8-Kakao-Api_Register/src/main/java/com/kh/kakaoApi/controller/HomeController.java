c2c2fc0c5b373568c3b79532075563d804ffbdf4
Kaye <139112573+2Mihye@users.noreply.github.com>
2024-01-12 19:08:19 +0900

    Api Login DB

--- springChap8-Kakao-Api_Register/src/main/java/com/kh/kakaoApi/controller/HomeController.java
+++ springChap8-Kakao-Api_Register/src/main/java/com/kh/kakaoApi/controller/HomeController.java
@@ -2,11 +2,14 @@ package com.kh.kakaoApi.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
 import com.kh.kakaoApi.service.KakaoUserService;
+import com.kh.kakaoApi.vo.KakaoUser;
 
+import jakarta.servlet.http.HttpSession;
 import lombok.RequiredArgsConstructor;
 
 @Controller
@@ -23,4 +26,13 @@ public class HomeController {
 		model.addAttribute("kakaoUrl", kakaoUserService.getKakaoLogin());
 		return "index";
 	}
+	
+	// 현재는 Kakao로만 로그인하지만 추후 sns인증을 거치지 않은 회원가입일 경우를 생각하여 GetMapping을 한 번에 적어주는 것 = 공통적인 로그인 부분은 홈으로 넣겠다는 의미
+	@GetMapping("/main")
+	public String home(Model model, HttpSession session) {
+		KakaoUser inUser = (KakaoUser) session.getAttribute("InUser");
+				model.addAttribute("InUser", inUser);
+		return "main";
+	}
+	
 }
--- springChap8-Kakao-Api_Register/src/main/java/com/kh/kakaoApi/controller/KakaoUserController.java
+++ springChap8-Kakao-Api_Register/src/main/java/com/kh/kakaoApi/controller/KakaoUserController.java
@@ -7,10 +7,12 @@ import org.springframework.web.bind.annotation.*;
 
 import com.kh.kakaoApi.common.MsgEntity;
 import com.kh.kakaoApi.dto.KakaoDTO;
+import com.kh.kakaoApi.repository.KakaoUserRepository;
 import com.kh.kakaoApi.service.KakaoUserService;
 import com.kh.kakaoApi.vo.KakaoUser;
 
 import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpSession;
 import lombok.RequiredArgsConstructor;
 
 
@@ -19,18 +21,33 @@ import lombok.RequiredArgsConstructor;
 @RequiredArgsConstructor
 public class KakaoUserController {
 	private final KakaoUserService kakaoUserService;
+	private final KakaoUserRepository kakaoUserRepository;
 	
 	@GetMapping("/callback")
 	// MsgEntity : 결과에 대한 내용을 담을 Entity
 	public String callback(HttpServletRequest request,
 			@RequestParam(required = false) String name,
-			@RequestParam(required = false) String birthdate,
-			Model model) throws Exception {
+			@RequestParam(required = false) String birthdate, Model model,
+			HttpSession session
+			) throws Exception {
 		// kakao/callback을 작성하면 JSON 형식으로 이동했지만 register html 파일로 이동하게 해줄 것
 		KakaoDTO kakaoInfo = kakaoUserService.getKakaoInfo(request.getParameter("code"), name, birthdate);
+		// kakao톡에서 인증한 이메일을 가져오는 것이지 DB에서 존재하는 email을 가져오는 게 아님
+		KakaoUser existingUser = kakaoUserRepository.findByEmail(kakaoInfo.getEmail());
+		
+		if(existingUser != null) {
+			session.setAttribute("InUser", existingUser);
+			return "redirect:/main";
+		}
+		
 		model.addAttribute("kakaoInfo", kakaoInfo);
 		return "register";
 	}
+	
+	
+
+	
+	
 	
 	// 프론트엔드에서 가지고오는 회원가입에 대한 결과를 전달하는 PostMapping
 	@PostMapping("/register")
